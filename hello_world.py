611b4e4708cc723c17489a872fb5206398d5d515
FDevBaig <62343089+FDevBaig@users.noreply.github.com>
2020-07-08 12:57:48 +0100

    Create hello_world.py
    Base code

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from flask import Flask
+
+PORT = 9090
+MESSAGE = ''' Hello, world, and I call you to Islam!\n'''
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    result = MESSAGE.encode("utf-8")
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=PORT)
