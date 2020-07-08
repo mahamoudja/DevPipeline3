ab37a158708b964b3d48a688d57f2094610e1974
FDevBaig <62343089+FDevBaig@users.noreply.github.com>
2020-07-08 13:20:16 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@
 from flask import Flask
 
 PORT = 9090
-MESSAGE = ''' Hello, world, and I call you to Islam!\n'''
+MESSAGE = ''' Alhamdulillah.  All Praise is due to Allah <br/>Hello, world, and I call you to Islam!\n'''
 
 app = Flask(__name__)
 
